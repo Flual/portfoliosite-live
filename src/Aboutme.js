8cdbd878fb31359bb502017cfc8d2e4fc05794ba
Florian <Flo.Stangl8@gmx.at>
2023-11-22 00:45:25 +0100

    Add files via upload

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -6,10 +6,10 @@ function Aboutme() {
         <h1>Über mich</h1>
 
         <div className="aboutmecontent" id="aboutmecontent">
-            <p>Ich bin ein Junnger Webdeveloper der seinen Einstieg 
-             in die Welt macht. <br /> Durch Diplom und Selbstudium kann ich eine breie basis an Skills vorweisen.
+            <p>Ich bin ein junger Webdeveloper der seinen Einstieg 
+             in die Welt macht. <br /> Durch Diplom und Selbstudium kann ich eine breite Basis an Skills vorweisen.
              Spezialisiert bin ich auf SPAs aber gerne auch als Aushilfe bei kleinen bis mitleren Unternehmen.
-             Wenn Sie meine Skills un Ausbildung genauer unter die Lupe Nhemen wollen geht es <a target='_blank' href='https://www.linkedin.com/in/florian-stangl-b31409271/'>HIER</a> zu meinem LinkedIn Profil.
+             Wenn Sie meine Skills un Ausbildung genauer unter die Lupe nehmen wollen geht es <a target='_blank' href='https://www.linkedin.com/in/florian-stangl-b31409271/'>HIER</a> zu meinem LinkedIn Profil.
             </p>
         </div>
 
